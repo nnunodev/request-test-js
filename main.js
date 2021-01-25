30ee06587b581aac2d937239bdaaa720e26d35a2
nnunodev <nunotnunes@outlook.com>
2021-01-25 19:23:37 +0000

    trying fetch ES6

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+const img = document.createElement('img');
+
+
 fetch('https://api.github.com/users')
 .then(res => res.json())
-.then(data => console.log(data));
+.then(data => {
+    h1.innerHTML = data[1].login;   
+    body.appendChild(img);
+    img.src = data[0].avatar_url;
+});
